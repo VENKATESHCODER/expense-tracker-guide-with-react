0c391c7436f11b227772510379ca57f5b7b6787e
VENKATESHCODER <122370232+VENKATESHCODER@users.noreply.github.com>
2023-04-28 23:09:05 +0530

    Add files via upload

---
+++ ExpenseItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from '../UI/Card';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+
+const  ExpenseItem = (props) => {
+  return (
+    <li>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__h2">{props.Location}</div>
+      </div>
+      <button>Delete title</button>
+    </Card>
+    </li>
+  );
+}
+
+export default ExpenseItem;
